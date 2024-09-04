6ac512d4cb8a89d346a231f15d6bdc51d0a40fd7
LukeS5310 <skywalker5310@gmail.com>
2024-09-04 13:22:48 +0300

    +Add round borders to items +Partially add haptic feedback to reorder *Refactoring

--- UnitPrice/UnitPriceList.cs
+++ UnitPrice/UnitPriceList.cs
@@ -1,4 +1,6 @@
 ﻿using System.Collections.ObjectModel;
+using CommunityToolkit.Maui.Behaviors;
+using Microsoft.Maui.Controls.Shapes;
 using UnitPrice.Model;
 
 namespace UnitPrice
@@ -29,68 +31,59 @@ namespace UnitPrice
 
             upCollection.ItemTemplate = new DataTemplate(() =>
             {
-                var emojiLabel = new Label { FontSize = 20, Margin = 10 , VerticalOptions = LayoutOptions.Center};
-                emojiLabel.SetBinding(Label.TextProperty, "EmojiName");
-                var nameLabel = new Label(){VerticalOptions = LayoutOptions.Center};
-                nameLabel.SetBinding(Label.TextProperty, "Name");
-                
-                var priceTb = new Entry();
-                priceTb.Keyboard = Keyboard.Numeric;
-                priceTb.SetBinding(Entry.TextProperty, "Price");
-                var priceTbDesc = new Label() { Text = "Цена", FontSize = 10, VerticalOptions = LayoutOptions.Center};
-                var priceStack = new VerticalStackLayout() { Children = { priceTbDesc, priceTb }, Margin = 10};
-                priceStack.Spacing = -12;
+                var emojiLabel = CreateEmojiLabel();
+                var nameLabel = CreateNameLabel();
+                var priceStack = CreatePriceStack();
+                var unitFractionStack = CreateFractionStack();
+                var priceForOneStack = CreatePriceForOneStack();
+                var deleteButton = CreateDeleteButton();
 
-                var unitFraction = new Entry();
-                unitFraction.Keyboard = Keyboard.Numeric;
-                unitFraction.SetBinding(Entry.TextProperty, "UnitFraction");
-                var unitFractionDesc = new Label { Text = "ед.", FontSize = 10, VerticalOptions = LayoutOptions.Center};
-                var unitFractionStack = new VerticalStackLayout()
-                    { Children = { unitFractionDesc, unitFraction }, Margin = 10};
-                unitFractionStack.Spacing = -12;
-
-                var priceForOne = new Label(){VerticalOptions = LayoutOptions.Center};
-                priceForOne.SetBinding(Label.TextProperty, "PriceForOne");
-                var priceForOneDesc = new Label()
-                    { Text = "За шт.", FontSize = 10, VerticalOptions = LayoutOptions.Center };
-                var priceForOneStack = new VerticalStackLayout() { Children = { priceForOneDesc, priceForOne }, Margin = 10, VerticalOptions = LayoutOptions.Center};
-                priceForOneStack.Spacing = 0;
-
-                var editButton = new Button
+                deleteButton.Clicked += async (sender, args) =>
                 {
-                    Text = "❌",
-                    Margin = 5,
-                    FontSize = 20,
-                    MinimumWidthRequest = 20
-                };
-                editButton.Clicked += async (sender, args) =>
-                {
-                    var answer = await DisplayAlert("Удаление", "Удалить выбранный элемент?","Нет","Да");
+                    var answer = await DisplayAlert("Удаление", "Удалить выбранный элемент?", "Нет", "Да");
                     if (answer) return;
 
                     var button = sender as Button;
 
-                    if(button.BindingContext is UnitPriceModel obj)
+                    if (button.BindingContext is UnitPriceModel obj)
                         items.Remove(obj);
                 };
 
-                var grid = new Grid(){BackgroundColor = Color.Parse("LightBlue"), Margin = 1};
-                
-                grid.Add(emojiLabel,0);
+                var grid = new Grid() { BackgroundColor = Color.Parse("LightBlue") };
+                grid.Add(emojiLabel, 0);
                 grid.SetRowSpan(emojiLabel, 2);
-                grid.Add(nameLabel,1);
-                grid.SetRowSpan(nameLabel,2);
-                grid.Add(priceStack,2);
-                grid.SetColumnSpan(priceStack,2);
-                grid.Add(unitFractionStack,2,1);
-                grid.SetColumnSpan(unitFractionStack,2);
-                grid.Add(priceForOneStack,4);
-                grid.SetColumnSpan(priceForOneStack,2);
-                grid.SetRowSpan(priceForOneStack,2);
-                grid.Add(editButton,6);
-                grid.SetRowSpan(editButton,2);
+                grid.Add(nameLabel, 1);
+                grid.SetRowSpan(nameLabel, 2);
+                grid.SetColumnSpan(nameLabel, 2);
+                grid.Add(priceStack, 3);
+                grid.SetColumnSpan(priceStack, 2);
+                grid.Add(unitFractionStack, 3, 1);
+                grid.SetColumnSpan(unitFractionStack, 2);
+                grid.Add(priceForOneStack,5);
+                grid.SetColumnSpan(priceForOneStack, 2);
+                grid.SetRowSpan(priceForOneStack, 2);
+                grid.Add(deleteButton, 7);
+                grid.SetRowSpan(deleteButton, 2);
                 grid.RowSpacing = -25;
-                return grid;
+
+                var reorderBehavior = new TouchBehavior()
+                {
+                    LongPressDuration = 500,
+                    LongPressCommand = new Command(() =>
+                    {
+                        Console.WriteLine("BZZ");
+                        HapticFeedback.Perform(HapticFeedbackType.LongPress);
+                    })
+                };
+
+                var border = new Border
+                {
+                    StrokeShape = new RoundRectangle() { CornerRadius = 15 }, Margin = 5,
+                    Content = grid
+                };
+                emojiLabel.Behaviors.Add(reorderBehavior);
+                nameLabel.Behaviors.Add(reorderBehavior); //TODO - expand haptic feedback on all elements of grid properly
+                return border;
             });
 
             var addButton = new Button()
@@ -107,7 +100,7 @@ namespace UnitPrice
             addButton.Clicked += async (sender, e) =>
             {
                 var name = await DisplayPromptAsync("Добавление", "Введите наименование:", "OK", "Отмена");
-                if(!string.IsNullOrWhiteSpace(name))
+                if (!string.IsNullOrWhiteSpace(name))
                     items.Add(new UnitPriceModel(name));
             };
 
@@ -117,6 +110,74 @@ namespace UnitPrice
             layout.SetRowSpan(upCollection, 2);
 
             Content = layout;
+        }
+
+        private static Button CreateDeleteButton()
+        {
+            var deleteButton = new Button
+            {
+                Text = "❌",
+                Margin = 5,
+                FontSize = 20,
+                MinimumWidthRequest = 20
+            };
+            return deleteButton;
+        }
+
+        private static VerticalStackLayout CreatePriceForOneStack()
+        {
+            var priceForOne = new Label() { VerticalOptions = LayoutOptions.Center };
+            priceForOne.SetBinding(Label.TextProperty, "PriceForOne");
+            var priceForOneDesc = new Label()
+                { Text = "За шт.", FontSize = 10, VerticalOptions = LayoutOptions.Center };
+            var priceForOneStack = new VerticalStackLayout
+            {
+                Children = { priceForOneDesc, priceForOne }, Margin = 10, VerticalOptions = LayoutOptions.Center,
+                Spacing = 0
+            };
+            return priceForOneStack;
+        }
+
+        private static VerticalStackLayout CreateFractionStack()
+        {
+            var unitFraction = new Entry();
+            unitFraction.Keyboard = Keyboard.Numeric;
+            unitFraction.SetBinding(Entry.TextProperty, "UnitFraction");
+            var unitFractionDesc = new Label { Text = "ед.", FontSize = 10, VerticalOptions = LayoutOptions.Center };
+            var unitFractionStack = new VerticalStackLayout
+            {
+                Children = { unitFractionDesc, unitFraction }, Margin = 10,
+                Spacing = -12
+            };
+            return unitFractionStack;
+        }
+
+        private static VerticalStackLayout CreatePriceStack()
+        {
+            var priceTb = new Entry();
+            priceTb.Keyboard = Keyboard.Numeric;
+            priceTb.SetBinding(Entry.TextProperty, "Price");
+            var priceTbDesc = new Label() { Text = "Цена", FontSize = 10, VerticalOptions = LayoutOptions.Center };
+            var priceStack = new VerticalStackLayout
+            {
+                Children = { priceTbDesc, priceTb }, Margin = 10,
+                Spacing = -12
+            };
+            return priceStack;
+        }
+
+        private static Label CreateNameLabel()
+        {
+            var nameLabel = new Label() { VerticalOptions = LayoutOptions.Center };
+            nameLabel.SetBinding(Label.TextProperty, "Name");
+            return nameLabel;
+        }
+
+        private static Label CreateEmojiLabel()
+        {
+            var emojiLabel = new Label { FontSize = 20, Margin = 10, VerticalOptions = LayoutOptions.Center };
+            emojiLabel.SetBinding(Label.TextProperty, "EmojiName");
+            return emojiLabel;
         }
     }
 }
